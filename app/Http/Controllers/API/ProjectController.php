e133a1b895ee31eccb4b26add0d5eb8b64bc352d
nicolocamassa <camassanicolo03@gmail.com>
2024-03-01 16:09:36 +0100

    [FIX] API controller moved into API folder

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -8,7 +8,7 @@ use App\Models\Project;
 class ProjectController extends Controller
 {
     public function index(){
-        $projects = Project::with('type', 'technology')->get();
+        $projects = Project::with('type', 'technology')->get(); /* TODO: aggiungere ->paginate() [BONUS] */
 
         return response()->json([
             'success' => true,
